26aa76f1633f5ba148eca7794f34123ffed88788
Evdokimov Dmitry <crazygoup777@gmail.com>
2023-02-15 18:48:14 +0300

    Добавьте файлы проекта.

---
+++ Module.9.Delegates/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Module._9.Delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
